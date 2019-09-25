ac0c0c3f64e4036165b569375f09633dfd5b7937
YaraRAA <33261257+YaraRAA@users.noreply.github.com>
2019-09-25 18:12:29 -0400

    Add files via upload

--- Creating_list_of_coordinate_systems.R
+++ Creating_list_of_coordinate_systems.R
@@ -10,3 +10,7 @@ library(rgdal)
 EPSG <- make_EPSG()
 NA1983<- EPSG[grep("NAD83", EPSG$note),]
 NA1983<- EPSG[grep("proj=longlat", NA1983$prj4),]
+
+WGS1984 <- EPSG[grep("WGS", EPSG$note),]
+
+E5070 <- EPSG[grep("5070", EPSG$code),]
